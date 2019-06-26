cb66204055bc97072333bd1ce645db8425ee6fdc
kkdosk <kkdosk@naver.com>
2019-06-27 00:25:41 +0900

    aa

---
+++ modernjavainaction/chap01/FilteringApples.java
@@ -0,0 +1,60 @@
+package modernjavainaction.chap01;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class FilteringApples {
+	public static void main(String... args) {
+		List<Apple> inventory = Arrays.asList(new Apple(80, "green"), new Apple(155, "green"), new Apple(120, "red"));
+		List<Apple> greenApples = filterApples(inventory, FilteringApples::isGreenApple);
+		System.out.println(greenApples);
+	}
+
+	public static List<Apple> filterApples(List<Apple> inventory, Predicate<Apple> p) {
+		List<Apple> result = new ArrayList<>();
+		for (Apple apple : inventory) {
+			if (p.test(apple)) {
+				result.add(apple);
+			}
+		}
+		return result;
+	}
+
+	public static boolean isGreenApple(Apple apple) {
+		return "green".equals(apple.getColor());
+	}
+
+	public static class Apple {
+		private int weight = 0;
+		private String color = "";
+
+		public Apple(int weight, String color) {
+			this.weight = weight;
+			this.color = color;
+		}
+
+		public int getWeight() {
+			return weight;
+		}
+
+		public void setWeight(int weight) {
+			this.weight = weight;
+		}
+
+		public String getColor() {
+			return color;
+		}
+
+		public void setColor(String color) {
+			this.color = color;
+		}
+
+		@Override
+		public String toString() {
+			return String.format("Apple{color='%s', weight=%d", color, weight);
+		}
+	}
+
+}
